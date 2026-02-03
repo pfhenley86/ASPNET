56eb3eead3be81fb7954c559b7201d4734d19371
“pfhenley86” <fordp86@gmail.com>
2026-02-03 16:06:43 -0500

    created DbConnection and started views

--- Testing/Models/ProductRepository.cs
+++ Testing/Models/ProductRepository.cs
@@ -1,11 +1,20 @@
 using System.Collections.Generic;
+using System.Data;
+using Dapper;
 
 namespace Testing.Models;
 
 public class ProductRepository : IProductRepository
 {
+    private readonly IDbConnection _conn;
+
+    public ProductRepository(IDbConnection conn)
+    {
+        _conn = conn;
+    }
+    
     public IEnumerable<Product> GetAllProducts()
     {
-        throw new System.NotImplementedException();
+        return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
     }
 }
