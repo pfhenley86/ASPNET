31bf2726cdc8d6022a3071e5a1211e8b7fa41495
“pfhenley86” <fordp86@gmail.com>
2026-02-04 14:56:16 -0500

    completed intro .NET MVC lesson

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
 
 namespace Testing.Controllers;
 
 public class ProductController : Controller
 {
+    private readonly IProductRepository repo;
+
+    public ProductController(IProductRepository repo)
+    {
+        this.repo = repo;
+    }
     // GET
     public IActionResult Index()
     {
-        return View();
+        var products = repo.GetAllProducts();
+        return View(products);
     }
 }
