6968d16e83263c8f5fb2e0a7cc1c0fe255ffe946
“pfhenley86” <fordp86@gmail.com>
2026-02-12 15:39:46 -0500

    added Category Model and Repository category methods

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Testing.Models;
 
 public class Product
@@ -13,4 +15,6 @@ public class Product
     public int CategoryID { get; set; }
     public int OnSale { get; set; }
     public int StockLevel { get; set; }
+    
+    public IEnumerable<Category> Categories { get; set; }
 }
